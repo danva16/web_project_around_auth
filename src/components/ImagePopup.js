3edf290cd71385f2f9e99f5c9b8044a6804b9236
Danva16 <danieladannava111116@gmail.com>
2023-12-19 01:15:50 -0600

    renderizacion condicional

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,9 +4,9 @@ function ImagePopup(props) {
   const card = props.selectedCardElement;
 
   return (
-    <div className={`popup ${card ? 'popup_mode_active' : ''}`}>
-        <img className="popup__image" src={card ? card.link : ''} alt={card ? card.name : ''} />
-        <h2 className="popup__title">{card ? card.name : ''}</h2>
+    <div className={`popup ${card && 'popup_mode_active'}`}>
+        <img className="popup__image" src={card && card.link} alt={card && card.name} />
+        <h2 className="popup__title">{card && card.name}</h2>
         <button type="button" className="button button_action_close" onClick={props.onClose}></button>
       </div>
   )
