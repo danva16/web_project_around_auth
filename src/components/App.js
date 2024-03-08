416aad79e4ad56553bd74ed0e4b86b97f05146db
Danva16 <danieladannava111116@gmail.com>
2024-03-08 01:31:26 -0600

    crear componente ProtectedRoute

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
--- src/components/Register.js
+++ src/components/Register.js
@@ -98,3 +98,5 @@ function Register() {
     </div>
   );
 };
+
+export default withRouter(Register);
